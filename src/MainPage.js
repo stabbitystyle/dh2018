1369bf7d8d80918d0a1267686aa5cd95a8fceff7
kyokitsune <ryanbreitenfeldt@gmail.com>
2018-10-21 02:18:30 -0700

    added mainpage

---
+++ src/MainPage.js
@@ -0,0 +1,16 @@
+import { Button } from 'react-bootstrap';
+import React, { Component } from 'react';
+
+
+class MainPage extends Component{
+    render(){
+        <ButtonToolbar>
+            <Button>Book shelf</Button>
+            <Button>Start</Button>
+            <Button>Options</Button>
+        </ButtonToolbar>
+    }
+
+}
+
+export default MainPage;
