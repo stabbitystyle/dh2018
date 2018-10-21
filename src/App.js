c42e24e694b9d4f18cb0280e827f36e208172be5
William Frank <stabbitystyle@gmail.com>
2018-10-20 22:45:33 -0700

    Added TopNavbar framework

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TopNavbar from './TopNavbar';
 import { Button } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <TopNavbar></TopNavbar>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
