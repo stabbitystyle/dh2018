e43246efd8f7b2fd60d85c56e194cd9fb9a53451
Emmanuel Bonilla <emmanuel.bonilla@wsu.edu>
2018-10-21 00:27:19 -0700

    Update src/App.js

--- src/App.js
+++ src/App.js
@@ -9,15 +9,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-<<<<<<< HEAD
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-=======
           <ImageCarousel></ImageCarousel>
           <BottomBar/>
->>>>>>> dev
           <a
             className="App-link"
             href="https://reactjs.org"
